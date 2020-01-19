f35085cc95e3208aba159dd2e7d5c357f3fb0511
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-19 12:20:04 -0500

    remove X

--- JS/index.js
+++ JS/index.js
@@ -8,13 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
     todo.style.background = "pink";
     todo.style.textDecoration = "line-through";
 
+    var removeBox = document.createElement("div");
+    removeBox.classname = "bob";
+    removeBox.innerHTML = "&#10005";
+    console.log(removeBox);
+
     var userContent = document.getElementById("new-item").value;
     var newLi = document.createElement("li");
     var addContent = document.createTextNode(userContent);
     newLi.appendChild(addContent);
-    document.getElementById("to-do-list").appendChild(newLi);
 
-    // document.getElementById("to-do-list").appendChild(li);
+
+
+
+
+
+    document.getElementById("to-do-list").appendChild(newLi);
+    document.getElementById("new-item").value="";
+
 
     console.log(userContent);
   });
