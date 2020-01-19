d6652def7fdadaf49133dc95458a8160c99e2903
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-19 11:00:29 -0500

    js add item

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
 
 
+  document.getElementById("submit-button").addEventListener("click", function(event){
+    event.preventDefault();
+    var todo = document.getElementById("hero");
+    todo.style.color = "#3655ff";
+    todo.style.background = "pink";
+    todo.style.textDecoration = "line-through";
+
+    var userContent = document.getElementById("new-item").value;
+    var newLi = document.createElement("li");
+    var bob = document.createTextNode(userContent);
+    newLi.appendChild(bob);
+    document.getElementById("to-do-list").appendChild(newLi);
+
+    // document.getElementById("to-do-list").appendChild(li);
+
+    console.log(userContent);
+  });
 
 
 
@@ -8,6 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-  
+
+
+
 
 });
