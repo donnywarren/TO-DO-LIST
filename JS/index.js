c552bd49b1f8191291094d6da10499575953771a
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-20 12:48:47 -0500

    fghfshrth

--- JS/index.js
+++ JS/index.js
@@ -9,10 +9,24 @@ document.addEventListener("DOMContentLoaded", function() {
   task.forEach(function(item, index) {
   var number = index + 1;
   var staticItem = item;
+  var checkSpan = document.createElement("span");
+  checkSpan.id = "s" + [index];
+  var removeDiv = document.createElement("div");
+  removeDiv.id = "d" + [index];
   var newLi = document.createElement("li");
   newLi.id = "l" + [index];
   newLi.innerHTML = item;
-  // document.getElementById("l" + [task.length - 1]);
+  newLi.prepend(checkSpan);
+  newLi.appendChild(removeDiv);
+
+
+
+
+
+  //
+  // document.getElementById("l" + [task.index]);
+  // console.log("l" + [task.index]);
+
   document.getElementById("to-do-list").appendChild(newLi);
 
 
