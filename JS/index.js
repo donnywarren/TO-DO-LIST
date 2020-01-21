c0045cec9d569c7c1cce753c01d61429852c8441
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-01-20 20:47:07 -0500

    works

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+
   var task = ["Laundry","Pay Bills"];
-  var item = task[1];
-
-
 
 
   task.forEach(function(item, index) {
-  var number = index + 1;
-  var staticItem = item;
+
   var checkSpan = document.createElement("span");
   checkSpan.id = "s" + [index];
   checkSpan.className = "check";
@@ -23,60 +20,66 @@ document.addEventListener("DOMContentLoaded", function() {
   newLi.prepend(checkSpan);
   newLi.appendChild(removeDiv);
 
-
-
-
-
-  //
-  // document.getElementById("l" + [task.index]);
-  // console.log("l" + [task.index]);
-
   document.getElementById("to-do-list").appendChild(newLi);
 
-
-
   });
-
-
 
   document.getElementById("submit-button").addEventListener("click", function(event){
     event.preventDefault();
 
     var userContent = document.getElementById("new-item").value;
-
     task.push(userContent);
 
-          console.log(task[task.length - 1]);
-
-
+    var newItem = task[task.length - 1];
+    var checkSpan = document.createElement("span");
+    checkSpan.id = "s" + [task.length - 1];
+    checkSpan.className = "check";
+    checkSpan.innerHTML = "&#10003";
+    var removeDiv = document.createElement("div");
+    removeDiv.id = "d" + [task.length - 1];
+    removeDiv.className = "ex";
+    removeDiv.innerHTML = "&#10005";
     var newLi = document.createElement("li");
-
-
-
-
-    newLi.innerHTML = "<span id='check'>&#10003</span>" + "  " + userContent + "<span class='ex'>&#10005</span>";
-    newLi.className = "item";
-    // newLi.appendChild(addContent);
-
-
+    newLi.id = "l" + [task.length - 1];
+    newLi.innerHTML = "  " + newItem;
+    newLi.prepend(checkSpan);
+    newLi.appendChild(removeDiv);
     document.getElementById("to-do-list").appendChild(newLi);
 
     // REMOVE USER CONTENT
     document.getElementById("new-item").value="";
 
+    console.log(task);
+  });
 
 
 
+  // document.getElementById("submit-button").addEventListener("click", function(event){
+  //   event.preventDefault();
+  //
+  //   var userContent = document.getElementById("new-item").value;
+  //
+  //   task.push(userContent);
 
 
+    // var newLi = document.createElement("li");
+    //
+    //
+    //
+    //
+    // newLi.innerHTML = "<span id='check'>&#10003</span>" + "  " + userContent + "<span class='ex'>&#10005</span>";
+    // newLi.className = "item";
+    // // newLi.appendChild(addContent);
+    //
+    //
+    // document.getElementById("to-do-list").appendChild(newLi);
 
 
-
-
-    var todo = document.getElementById("hero");
-    todo.style.color = "#3655ff";
-    todo.style.background = "pink";
-    todo.style.textDecoration = "line-through";
+    //
+    // var todo = document.getElementById("hero");
+    // todo.style.color = "#3655ff";
+    // todo.style.background = "pink";
+    // todo.style.textDecoration = "line-through";
 
     // var removeBox = document.createElement("span");
     // removeBox.className = "ex";
@@ -87,18 +90,18 @@ document.addEventListener("DOMContentLoaded", function() {
 // document.getElementById("item").appendChild(removeBox);
 
 
-      document.getElementById("check").addEventListener("click", markBlack);
-
-      function markBlack() {
-        document.getElementById("check").style.color="#111";
-
-
-      }
+      // document.getElementById("check").addEventListener("click", markBlack);
+      //
+      // function markBlack() {
+      //   document.getElementById("check").style.color="#111";
+      //
+      //
+      // }
 
 
     //
     // console.log(userContent);
-  });
+
 
 
 
